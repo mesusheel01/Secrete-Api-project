8a5fecbbf9f90bac0d381f599f1b31512028567c
Susheel <susheelrai96@gmail.com>
2024-04-02 12:13:12 +0530

    Added authentication with username and password!

--- index.js
+++ index.js
@@ -1,33 +1,89 @@
-// HINTS:
-// 1. Import express and axios
-import express from "express"
-import axios from "axios"
+import express from "express";
+import bodyParser from "body-parser";
+import  pg  from "pg";
+import bcrypt from 'bcrypt';
 
-// 2. Create an express app and set the port number.
 const app = express();
-const port = 4000;
+const port = 3000;
+const saltRound = 10;
+
+const db = new pg.Client({
+  user:"postgres",
+  host: "localhost",
+  database:"secrets",
+  password:"passme",
+  port: 5432, 
+})
+// connect database 
+db.connect();
 
 
-// 3. Use the public folder for static files.
+app.use(bodyParser.urlencoded({ extended: true }));//just write thismiddle ware to hold the request body 
 app.use(express.static("public"));
 
-// 4. When the user goes to the home page it should render the index.ejs file.
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
-app.get("/", async (req,res)=>{
-    try{
-        const result = await axios.get("https://secrets-api.appbrewery.com/random");
-        res.render("index.ejs",{ 
-            secret: result.data.secret,
-            user: result.data.username,
-    })}catch (error){
-        console.log(error.message);
-        res.status(500);
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  try{
+    const findUser = await db.query("Select * from users where email = $1 ", [username])
+    if(findUser.rows.length > 0) {
+      res.send("User already exits!");
+    }else{
+      bcrypt.hash(password, saltRound,async function(err, hash) {
+// Store hash in your password DB.
+if(err) console.log(err)
+else{
+  const users = await db.query("Insert into users (email,password) values($1,$2)", [username, hash]);
+  console.log("User Registered!",users)
+  res.render("secrets.ejs");
+}
+      });
     }
-})
+      //after creation or you can say basic authentication you can show the secrts
+    }catch(err){
+      console.log("error:",err)
+    }
+});
 
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try{
+    const findUser = await db.query("Select * from users where email = $1 ", [username])
+    if(findUser.rows.length > 0){
+     const user = findUser.rows[0];
+     const storedPassword = user.password
+     bcrypt.compare(password, storedPassword, function(err, result) {
+      if(err) console.log("Error in password comparision: ", err);
+      else{
+        if(result){
+          res.render("secrets.ejs");
+        }else{
+          console.log("Wrong password!");
+        }
+      }
+  });
+  }else{
+    res.send("User not found");
+  }
+}
+  catch(err){
+      console.log("Error : ", err);
+  }
+});
 
-// 6. Listen on your predefined port and start the server.
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
